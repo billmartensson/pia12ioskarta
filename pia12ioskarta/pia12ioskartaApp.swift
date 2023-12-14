5a887f0b58d9f418bb626025150321c4fca90258
Bill Mårtensson <bill@magictechnology.se>
2023-12-14 09:12:25 +0100

    Initial Commit

---
+++ pia12ioskarta/pia12ioskartaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pia12ioskartaApp.swift
+//  pia12ioskarta
+//
+//  Created by BillU on 2023-12-14.
+//
+
+import SwiftUI
+
+@main
+struct pia12ioskartaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
